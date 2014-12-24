0584b6a165da1725e15959600cde18d9c6bb3944
huseincihad <cihad_guler@hotmail.com>
2014-12-24 20:30:40 +0200

    Deneme2

--- app/src/main/java/com/creative/thinkingeverything/app/MainActivity.java
+++ app/src/main/java/com/creative/thinkingeverything/app/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);// commennnnnnntttt
+        setContentView(R.layout.activity_main);
     }
 
 
