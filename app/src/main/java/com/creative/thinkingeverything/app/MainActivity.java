f4f16da63fb8f4faa569d9021d341132ebcdc766
huseincihad <cihad_guler@hotmail.com>
2014-12-24 21:01:26 +0200

    Kısa Yol Denemesi

--- app/src/main/java/com/creative/thinkingeverything/app/MainActivity.java
+++ app/src/main/java/com/creative/thinkingeverything/app/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {//uie
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
